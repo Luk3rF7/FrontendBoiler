afc8dcb99b3ebce7f81586515427222e64d5a1a6
Luk3rF7 <lkzeron@gmail.com>
2023-10-28 09:48:52 -0300

    feat:  layoutbase

--- cardproduto/script.js
+++
@@ -1,6 +0,0 @@
-// Exemplo simples de como adicionar um evento de clique para o botão "Adicionar ao Carrinho"
-const addToCartButton = document.querySelector('.add-to-cart');
-addToCartButton.addEventListener('click', function () {
-  // Aqui você pode adicionar o produto ao carrinho ou realizar a ação desejada.
-  alert('Produto adicionado ao carrinho!');
-});
